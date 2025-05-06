dc901d6b2e71921198a97ecc9eb1e4c79e1b713d
Gaurav Singh <gv00001singh@gmail.com>
2025-05-06 10:57:37 +0530

    writeFile

--- app.js
+++ app.js
@@ -3,7 +3,14 @@ const express= require('express')
 const app=express()
 const fs= require('fs/promises')
 
-app.get('/', async(req,res)=>{
+
+const code =`
+bitch please this is my life
+const function add(a,b){
+return res= a+b
+}
+`
+app.get('/read', async(req,res)=>{
     try {
         const result= await fs.readFile('hello.txt','utf-8')
         res.send(result)
@@ -13,6 +20,14 @@ app.get('/', async(req,res)=>{
         console.log(error);
         
     }    
+})
+
+
+app.get('/write', async(req,res)=>{
+    const result= await fs.writeFile('write.txt',code)
+    res.send(result)
+    console.log(result);
+    
 })
 
 app.listen(6000, ()=>{
